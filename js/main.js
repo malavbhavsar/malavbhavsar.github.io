e4e124ad25a66a64f99818ad03e8108706861dab
Malav Bhavsar <mbhavsar@rocketfuel.com>
2016-01-24 15:42:37 -0800

    Intial commit for malav.io

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 layout: null
 ---
 $(document).ready(function () {
+  /*
+  Coming Soon
   $('a.blog-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
@@ -13,6 +15,7 @@ $(document).ready(function () {
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   })
+  */
 
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
