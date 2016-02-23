76c2fbe6f887025925eb8fb60c23d506660f1d8e
Malav Bhavsar <mbhavsar@rocketfuel.com>
2016-02-22 21:47:10 -0800

    Import posts from old blog and point to that

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@
 layout: null
 ---
 $(document).ready(function () {
-  /*
-  Coming Soon
   $('a.blog-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
@@ -15,7 +13,6 @@ $(document).ready(function () {
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   })
-  */
 
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
